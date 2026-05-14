fbdaf7e8be3cb3abd3f7c42d5ec16609ccb2de1c
kenta-crv <okuyamakenta@gmail.com>
2026-05-14 22:58:49 +0900

    best pillaer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :check_trial_expiration
   before_action :set_robots_meta
-
   before_action :init_breadcrumbs
   helper_method :breadcrumbs
 
