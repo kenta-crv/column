5b45495f58a28630d66e21b5e8d11edc22c80404
kenta-crv <okuyamakenta@gmail.com>
2026-06-21 22:23:14 +0900

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3002'
+    # ローカル環境のあらゆるポートからのリクエストを許可
+    origins '*', /localhost:\d+/, /127.0.0.1:\d+/
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['X-API-Key'],
+      credentials: false
   end
 end
